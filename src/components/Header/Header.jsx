9d2ca78d58e9f415db839e0380854e378d8ffa7c
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-07-20 07:56:21 +0300

    add logo and icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,7 @@
 import './Header.css';
 
 function Header() {
-
-	return (
-		<>Logo</>
-	);
+	return <img className="logo" src="/logo.svg" alt="Логотип журнала" />;
 }
 
 export default Header;
--- src/components/JournalAddButton/JournalAddButton.jsx
+++ src/components/JournalAddButton/JournalAddButton.jsx
@@ -5,7 +5,11 @@ function JournalAddButton() {
 
 	return (
 		<CardButton className="journal-add">
-			Новое воспоминание
+			<svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M10 4.96265V16.6293" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+				<path d="M4.16669 10.796H15.8334" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+			</svg>
+      Новое воспоминание
 		</CardButton>
 	);
 }
