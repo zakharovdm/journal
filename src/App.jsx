22c93571da281927f79e882e0921062accc91056
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-07-25 06:57:14 +0300

    add conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,37 @@ import Body from './layouts/Body/Body';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
 import JournalList from './components/JournalList/JournalList';
-import JournalItem from './components/JournalItem/JournalItem';
 import JournalForm from './components/JournalForm/JournalForm';
-import CardButton from './components/CardButton/CardButton';
+
 import './App.css';
 
 const INITIAL_DATA = [
-	{
-		id: 1,
-		title: 'Подготовка к обновлению курсов',
-		text: 'Горные походы открывают удивительные природные ландшафты',
-		date: new Date()
-	},
-	{
-		id: 2,
-		title: 'Поход в годы',
-		text: 'Думал, что очень много времени',
-		date: new Date()
-	}
+	// {
+	// 	id: 1,
+	// 	title: 'Подготовка к обновлению курсов',
+	// 	text: 'Горные походы открывают удивительные природные ландшафты',
+	// 	date: new Date()
+	// },
+	// {
+	// 	id: 2,
+	// 	title: 'Поход в годы',
+	// 	text: 'Думал, что очень много времени',
+	// 	date: new Date()
+	// }
 ];
 function App() {
 	const [posts, setPosts] = useState(INITIAL_DATA);
 
 	const addPost = (post) => {
-		setPosts(oldPosts => [...oldPosts, {
-			text: post.text,
-			title: post.title,
-			date: new Date(post.date),
-			id: Math.max(...oldPosts.map(i => i.id)) + 1
-		}]);
+		setPosts((oldPosts) => [
+			...oldPosts,
+			{
+				text: post.text,
+				title: post.title,
+				date: new Date(post.date),
+				id: oldPosts.length > 0 ? Math.max(...oldPosts.map((i) => i.id)) + 1 : 1
+			}
+		]);
 	};
 
 	return (
@@ -40,22 +42,10 @@ function App() {
 			<LeftPanel>
 				<Header />
 				<JournalAddButton />
-				<JournalList>
-					{posts.map((el) => {
-						return (
-							<CardButton key={el.id}>
-								<JournalItem
-									title={el.title}
-									text={el.text}
-									date={el.date}
-								/>
-							</CardButton>
-						);
-					})}
-				</JournalList>
+				<JournalList items={posts} />
 			</LeftPanel>
 			<Body>
-				<JournalForm  onSubmit={addPost}/>
+				<JournalForm onSubmit={addPost} />
 			</Body>
 		</div>
 	);
