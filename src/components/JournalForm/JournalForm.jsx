a836f3c82a5ff0948ea839bc84d078a8d867ae5c
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-07-29 07:30:10 +0300

    refactor dynamic change styles

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -46,10 +46,10 @@ function JournalForm({ onSubmit }) {
 
 	return (
 		<form className="journal-form" onSubmit={addJournalItem}>
-			<input type="text" name="title" style={{border: formValidState.title ? undefined : '1px solid red'}}/>
-			<input type="date" name="date" style={{border: formValidState.date ? undefined : '1px solid red'}} />
+			<input type="text" name="title" className={`input ${formValidState.title ? '' : 'invalid'}`} />
+			<input type="date" name="date" className={`input ${formValidState.date ? '' : 'invalid'}`} />
 			<input type="text" name="tag" />
-			<textarea name="text" cols="30" rows="10" style={{border: formValidState.text ? undefined : '1px solid red'}}></textarea>
+			<textarea name="text" cols="30" rows="10" className={`input ${formValidState.text ? '' : 'invalid'}`}></textarea>
 			<Button text="Сохранить" />
 		</form>
 	);
